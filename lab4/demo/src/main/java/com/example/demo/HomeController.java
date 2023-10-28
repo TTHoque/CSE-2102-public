d012a7d3402b9ab9e6f0d75b0c972d661c3220ef
tth19002-vm <tahera.hoque@uconn.edu>
2023-10-27 23:47:48 -0400

    lab4

---
+++ lab4/demo/src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller 
+public class HomeController {
+
+  @GetMapping("/")
+  public String home() {
+    return "login";
+  }
+
+  @GetMapping("/topic")
+  public String notHome() {
+    return "topic";
+  }
+
+  @GetMapping("/test")
+  public String testPage() {
+    return "test";
+  }
+
+}
